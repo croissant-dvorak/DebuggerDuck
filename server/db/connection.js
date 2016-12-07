560e1ffd538a90ca1554fe7f34c114dc0285aa4c
Casey Billman <caseybillman@gmail.com>
2016-12-07 15:48:31 -0600

    update connection for new db

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,13 +2,16 @@ const mongoose = require ('mongoose');
 
 //This must be called because mongoose's promises library is deprecated
 mongoose.Promise = global.Promise;
- 
-const mongodbUri = 'mongodb://Robin:robin1@ds119588.mlab.com:19588/debuggerduck';
+
+const mongodbUri = 'mongodb://'+require('./config.js').mlObj.username+':'+require('./config.js').mlObj.pw+'@ds127998.mlab.com:27998/grabmeacroissant';
 mongoose.connect(mongodbUri);
 
 const conn = mongoose.connection;   
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
- 
+conn.once('open', function () {
+  console.log('Mongodb connection open');
+});
+
 module.exports = conn;
 
 //FYI, if you want play with the data in the command line, use this:
