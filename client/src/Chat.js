64ecda43e8c8964d13a8bdc62b36b62ae3d106cc
Bennett Staley <me@bennettstaley.com>
2016-12-10 15:19:32 -0600

    WORKING SOCKETS

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -12,11 +12,10 @@ class Chat extends Component {
   constructor(props) {
     super(props);
     console.log('prrrrrrrrrro', this.props)
-    socket.emit('createRoom', this.props.group._id)
     this.state = {
-      messages: []
+      messages: [],
+      socket: socket
     };
-    socket.on('addMessage', this.addMessageFn.bind(this) )
     this.getChatForGroupId(this.props.group._id);
     // this.getChatForGroupId(this.props.group._id);
   }
@@ -43,7 +42,8 @@ class Chat extends Component {
 
 
   submitMessage(event) {
-    socket.emit('addMessage', {
+    this.state.socket.emit('addMessage', {
+      roomId: this.props.group._id,
       user_id: this.props.user._id,
       userName: this.props.user.userName,
       picture: this.props.user.picture,
@@ -67,7 +67,13 @@ class Chat extends Component {
       });
   }
 
+  componentDidMount() {
+    this.state.socket.emit('createRoom', this.props.group._id)
+    this.state.socket.on('addMessage', this.addMessageFn.bind(this) )
+  }
+
   render() {
+    this.state.socket.emit('createRoom', this.props.group._id)
     return (
     <div className="chatBox">
       <div className="messagesBox">
