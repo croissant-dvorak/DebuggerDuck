e3e18a414a3a6d617976be5b5e513033a295c5c0
Carolyn Commons <crcommons@gmail.com>
2016-12-10 15:34:52 -0600

    rendered unique key to each group for sharing

--- client/src/VolunteerRequestsContainer.js
+++ client/src/VolunteerRequestsContainer.js
@@ -12,6 +12,7 @@ class VolunteerRequestContainer extends Component {
   render() {
     return (
       <div className="display">
+        <div>Share this key with the people you like enough to get food for: {this.props.group._id}</div>
         <Chat user={this.props.user} group={this.props.group} />
         <PickUpOffers user={this.props.user} group={this.props.group} selectDifferentGroup={this.props.selectDifferentGroup}  />
       </div>
